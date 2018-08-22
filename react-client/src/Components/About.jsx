e9b16f0ed60c0f6e47b68a24cbd54d479aa84733
MBaucas <mjbaucas@gmail.com>
2018-08-22 02:13:57 -0400

    Placed text in proper containers

--- react-client/src/Components/About.jsx
+++ react-client/src/Components/About.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Row, Grid, Col } from 'react-bootstrap';
+import { Row, Grid, Col, Panel, PageHeader } from 'react-bootstrap';
 
 
 class About extends Component {
@@ -7,28 +7,37 @@ class About extends Component {
         return (
             <Grid>
                 <Col>
-                    <h1>Who We Are</h1>
-                    <p>The GryPhone Lab is an open-access smartphone testbed at the University of Guelph. 
-                    Out goal is to explore and develop high-impact applicaitons of mobile technology in a  
-                    range of disciplines, bringing together people capable of programming these devices 
-                    with others who can make new and novel uses of them.</p>
-                    <p>The GryPhone Lab is run by Dr. Petros Spachos and developed by graduate and undergraduate 
-                    students in the University of Guelph at the School of Engineering. You can find us in Richards 
-                    Building 301B, and on Facebook.</p>
-                </Col>
-                <Col> 
-                    <h1>What We Do</h1>
-                    <p>The GryPhone Lab is a smartphone testbed designed to neable mobile systems research 
-                    that cannot be done in any other way. We enable researchers to instrument and experiment 
-                    with the android platform itself, a million-line codebase that runs on billions of devices 
-                    worldwide.</p>
-                    <p>The GryPhone Lab supports short-term projects in a graduate course open to all graduate 
-                    students at the University of Guelph, and engages in long-term collaborative research projects. 
-                    Feel free to contact us if you have a great new idea for Android platform features.</p>
+                    <PageHeader>About</PageHeader>
                 </Col>
                 <Col>
-                    <h1>Our Sponsors</h1>
-                    <p>We gratefully thank our sponsors for their support.</p>
+                    <Panel>
+                    <Panel.Heading>Who We Are</Panel.Heading>
+                    <Panel.Body>The GryPhone Lab is an open-access smartphone testbed at the University of Guelph. 
+                    Out goal is to explore and develop high-impact applicaitons of mobile technology in a  
+                    range of disciplines, bringing together people capable of programming these devices 
+                    with others who can make new and novel uses of them.<br></br>
+                    The GryPhone Lab is run by Dr. Petros Spachos and developed by graduate and undergraduate 
+                    students in the University of Guelph at the School of Engineering. You can find us in Richards 
+                    Building 301B, and on Facebook.</Panel.Body>
+                    </Panel>
+                </Col>
+                <Col> 
+                    <Panel>
+                    <Panel.Heading>What We Do</Panel.Heading>
+                    <Panel.Body>The GryPhone Lab is a smartphone testbed designed to neable mobile systems research 
+                    that cannot be done in any other way. We enable researchers to instrument and experiment 
+                    with the android platform itself, a million-line codebase that runs on billions of devices 
+                    worldwide.<br></br>
+                    The GryPhone Lab supports short-term projects in a graduate course open to all graduate 
+                    students at the University of Guelph, and engages in long-term collaborative research projects. 
+                    Feel free to contact us if you have a great new idea for Android platform features.</Panel.Body>
+                    </Panel>
+                </Col>
+                <Col>
+                    <Panel>
+                    <Panel.Heading>Our Sponsors</Panel.Heading>
+                    <Panel.Body>We gratefully thank our sponsors for their support.</Panel.Body>
+                    </Panel>
                 </Col>
             </Grid>
         )
--- react-client/src/Components/Experiments.jsx
+++ react-client/src/Components/Experiments.jsx
@@ -1,43 +1,51 @@
 import React, { Component } from 'react';
-import { Row, Grid, Col } from 'react-bootstrap';
+import { Row, Grid, Col, Panel, PageHeader, Well } from 'react-bootstrap';
 
 
 class Experiments extends Component {
-  render() {
-    return (
-      <Grid> 
-        <Col>  
-        <h1>Experiments</h1>
-        <p>The GryPhone Lab is a public smartphone testbed open to researchers and University studetns. 
-        We invite researchers to use our testbed to made possible their idea. The Gryphone Lab has facilitated 
-        the following smartphone experiments.</p>
-        </Col>
-        <Col>
-          <h3>HeartBeat Monitoring</h3>
-          <p>This experiment monitors the heart rate of a user through a smartphone application. 
-          The main objective is to analyze electrocardiogram data gathered by the phone's sensors 
-          to provide personalized heart monitoring.</p>            
-        </Col>
-        <Col>
-          <h3>Drone Control</h3>
-          <p>We examine the use of smartphones along with drones to provide connectivity in emergency 
-          situation. A smartphone application can control the drones and provide temporary connectivity</p>            
-        </Col>
-        <Col>
-          <h3>Quality of Experience on Smartphones</h3>
-          <p>In this project the student will work with on the Quality of Experience on Smartphones. 
-          Specifically, through a number of experiments with smartphone devices of different characteristics 
-          (different screen size, screen resolution etc.) the student will evaluate the user experience 
-          during a video session. The main goal of the project is to examine the effects of the device 
-          characteristics on the user video experience.</p>            
-        </Col>
-        <Col>
-          <h3>More to Come!</h3>
-          <p>Have a great idea? We can help you turn it into a successful project!</p>
-        </Col>
-      </Grid>
-    )
-  }
+    render() {
+        return (
+            <Grid> 
+                <Col>
+                    <PageHeader>Experiments</PageHeader>
+                    <Well>The GryPhone Lab is a public smartphone testbed open to researchers and University studetns. 
+                    We invite researchers to use our testbed to made possible their idea. The Gryphone Lab has facilitated 
+                    the following smartphone experiments.</Well>
+                </Col>
+                <Col>
+                    <Panel>  
+                        <Panel.Heading>HeartBeat Monitoring</Panel.Heading>
+                        <Panel.Body>This experiment monitors the heart rate of a user through a smartphone application. 
+                        The main objective is to analyze electrocardiogram data gathered by the phone's sensors 
+                        to provide personalized heart monitoring.</Panel.Body>            
+                    </Panel>
+                </Col>
+                <Col>
+                    <Panel>
+                        <Panel.Heading>Drone Control</Panel.Heading>
+                        <Panel.Body>We examine the use of smartphones along with drones to provide connectivity in emergency 
+                        situation. A smartphone application can control the drones and provide temporary connectivity</Panel.Body>            
+                    </Panel>
+                </Col>
+                <Col>
+                    <Panel>
+                        <Panel.Heading>Quality of Experience on Smartphones</Panel.Heading>
+                        <Panel.Body>In this project the student will work with on the Quality of Experience on Smartphones. 
+                        Specifically, through a number of experiments with smartphone devices of different characteristics 
+                        (different screen size, screen resolution etc.) the student will evaluate the user experience 
+                        during a video session. The main goal of the project is to examine the effects of the device 
+                        characteristics on the user video experience.</Panel.Body>            
+                    </Panel>
+                </Col>
+                <Col>
+                    <Panel>
+                        <Panel.Heading>More to Come!</Panel.Heading>
+                        <Panel.Body>Have a great idea? We can help you turn it into a successful project!</Panel.Body>
+                    </Panel>
+                </Col>
+            </Grid>
+        )
+    }
 }
 
 export default Experiments;
