d270c5afac2bdf3e93064c54ab09ec2319adeb61
MBaucas <mjbaucas@gmail.com>
2018-08-30 14:29:30 -0400

    Tested some panel designs

--- react-client/src/Components/HomePage.jsx
+++ react-client/src/Components/HomePage.jsx
@@ -2,6 +2,35 @@ import React, { Component } from 'react';
 import { Jumbotron, Button, Grid, Carousel, Row, Col, Panel } from 'react-bootstrap';
 
 class HomePage extends Component {
+    constructor(props){
+        super(props);
+
+        this.renderProjectList = this.renderProjectList.bind()
+    }
+    renderProjectList() {
+        var projectList = [
+            {'name': 'HearBeat Monitoring', 'decription': "A heart rate monitor through a smartphone application... "},
+            {'name': 'Drone Control', 'decription': "Testing of smartphones along with drones to provide connectivity in emergency situation..."}
+        ];
+        
+        return (
+            <div>
+            {projectList.map(function(obj, id){
+                return (
+                    <Panel>
+                        <Panel.Heading>
+                            <Panel.Title toggle>{obj.name}</Panel.Title>
+                        </Panel.Heading>
+                        <Panel.Body collapsible>
+                            {obj.decription}
+                        </Panel.Body>
+                    </Panel>
+                )
+            })}
+            </div>
+        )
+    }
+
     render() {
         return (
             <Grid className="home-wrapper">
@@ -16,14 +45,19 @@ class HomePage extends Component {
                         </p>
                     </Jumbotron></Col>
                 </Row>
-                <Row>
-                    <Col xs={3} md={3}>
-                        <Panel><Panel.Body>
-                            Random Content Number 1
-                        </Panel.Body></Panel>
+                <Row className="row-eq-height">
+                    <Col xs={12} sm={4} md={4} className="col-left">
+                        <Panel bsStyle="home">
+                            <Panel.Heading>
+                                Current Projects
+                            </Panel.Heading>
+                            <Panel.Body>
+                                {this.renderProjectList()}
+                            </Panel.Body>
+                        </Panel>
                     </Col>
-                    <Col xs={9} md={9}>
-                        <Panel><Panel.Body>
+                    <Col xs={12} sm={8} md={8} className="col-right">
+                        <Panel bsStyle="home"><Panel.Body>
                             Random Content Number 2
                         </Panel.Body></Panel>
                     </Col>
